101fdb3f4f15ed579d8a830389d7da42469e0f36
rivenblades <rivenblades@gmail.com>
2019-05-22 14:46:54 -0700

    [+]Image rendering

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#define IMG_PATH "flstudio_logo.jpeg"
+
+const int width = 1280;
+const int height = 920;
 
 int main (int argc, char** argv)
 {
@@ -7,8 +13,8 @@ int main (int argc, char** argv)
     (
         "Jeu de la vie", SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        640,
-        480,
+        width,
+        height,
         SDL_WINDOW_SHOWN
     );
 
@@ -38,9 +44,36 @@ int main (int argc, char** argv)
     // Render the rect to the screen
     SDL_RenderPresent(renderer);
 
-    // Wait for 5 sec
-    SDL_Delay( 5000 );
-
+    int w, h; // texture width & height
+    // load our image
+	SDL_Texture *img = IMG_LoadTexture(renderer, IMG_PATH);
+	SDL_QueryTexture(img, NULL, NULL, &w, &h); // get the width and height of the texture
+	// put the location where we want the texture to be drawn into a rectangle
+	// I'm also scaling the texture 2x simply by setting the width and height
+	SDL_Rect texr; texr.x = width/2; texr.y = height/2; texr.w = w; texr.h = h; 
+   // main loop
+	while (1) {
+		
+		// event handling
+		SDL_Event e;
+		if ( SDL_PollEvent(&e) ) {
+			if (e.type == SDL_QUIT)
+				break;
+			else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE)
+				break;
+		} 
+		
+		// clear the screen
+		SDL_RenderClear(renderer);
+		// copy the texture to the rendering context
+		SDL_RenderCopy(renderer, img, NULL, &texr);
+		// flip the backbuffer
+		// this means that everything that we prepared behind the screens is actually shown
+		SDL_RenderPresent(renderer);
+		
+	}
+    SDL_DestroyTexture(img);
+	SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
